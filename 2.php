70b28819e2882ab72210e06d5302ec4682084322
Taivo <taivo.nurk@khk.ee>
2020-11-24 09:50:17 +0200

    Test 2

--- 2.php
+++ 2.php
@@ -24,6 +24,45 @@ echo "${enimi}il on kiire auto!<br>";
 //jutumärkide kuvamine
 echo '"Tere tulemast Eestisse"<br>';
 echo "Shakespeare'i teosed<br>";
+echo "\"It's My Life\" - Dr. Alban<br><br>";
+
+
+echo "(\(\\<br>";
+echo "(-.-)<br>";
+echo "o_('')('')<br><br>";
+
+//aritmeetilised operaatorid
+$x = 8;
+$y = 2;
+$liitm = $x + $y;
+$lahut = $x - $y;
+$korru = $x * $y;
+$jagam = $x / $y;
+$jaak = $x % $y;
+echo "Vastused: $liitm, $lahut, $korru, $jagam, $jaak <br>";
+
+$a = 10;
+$a++; //sama mis $a = $a + 1
+echo $a.'<br>';
+
+$nimi = 'Mari ';
+$pnimi = 'Kisakõri';
+$nimi .= $pnimi;
+echo $nimi.'<br>';
+
+//muutuja väärtuste vormindamine
+$nimi = 'Mari';
+$vanus = 27;
+printf('%s on %s aastane. <br><br>', $nimi, $vanus);
+
+//kaks komakohta peale vastust
+$i = 7.26;
+$j = 2.369;
+$k = $i / $j;
+echo 'A = 7.26<br>';
+echo 'B = 2.369<br>';
+printf('A/B = %0.2f', $k);
+
 
 
 
