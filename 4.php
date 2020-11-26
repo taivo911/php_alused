cfb97aaada1effe7ebde0ef0145d21b5cc289908
Taivo <taivo.nurk@khk.ee>
2020-11-26 11:34:14 +0200

    Ülesanne 3 lahendus

---
+++ 4.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>06 - PHP - Vormid</title>
+</head>
+<body>
+    <h1>Pood OÜ</h1>
+    <form action="tellimine.php" method="get">
+a = <input type="number" name="t1"><br>
+b = <input type="number" name="t2"><br>
+c = <input type="number" name="t3"><br>
+        <input type="submit" value="Saada">
+    </form>
+<?php
+//ülesanne 3, Taivo Nurk 26.11
+
+/* Küsi kasutajalt vajalikud parameetrid ning leia:
+trapetsi pindala
+rombi ümbermõõt
+Väljasta andmed täislausega ja ümarda 1 komakoht
+Väljakontroll HTML vormile
+*/?>
+</body>
+</html>
