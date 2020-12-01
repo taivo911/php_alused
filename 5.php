dbee74581aff19e3282914b1cb9a0feb55771e89
Taivo <taivo.nurk@khk.ee>
2020-12-01 08:58:05 +0200

    Ülesanne 3 lahendus valmis

--- 5.php
+++ 5.php
@@ -5,21 +5,13 @@
     <title>Trapets/Romb</title>
 </head>
 <body>
-<h3>Trapets/Romb</h3>
+<h3>Trapetsi pindala</h3>
 <form action="arvutus.php" method="get">
-    a = <input type="number" name="t1"><br>
-    b = <input type="number" name="t2"><br>
-    h = <input type="number" name="t3"><br>
+    Trapets a = <input type="number" name="t1"><br>
+    Trapets b = <input type="number" name="t2"><br>
+    Trapets h = <input type="number" name="t3"><br>
+    Rombi külg = <input type="number" name="t4"><br>
     <input type="submit" value="Saada">
 </form>
-<?php
-//ülesanne 3, Taivo Nurk 26.11
-
-/* Küsi kasutajalt vajalikud parameetrid ning leia:
-trapetsi pindala
-rombi ümbermõõt
-Väljasta andmed täislausega ja ümarda 1 komakoht
-Väljakontroll HTML vormile
-*/?>
 </body>
 </html>
--- arvutus.php
+++ arvutus.php
@@ -1,14 +1,28 @@
 <h1>Vastused</h1>
 
 <?php
+//ülesanne 3, Taivo Nurk 26.11/01.12.
+
+/* Küsi kasutajalt vajalikud parameetrid ning leia:
+trapetsi pindala
+rombi ümbermõõt
+Väljasta andmed täislausega ja ümarda 1 komakoht
+Väljakontroll HTML vormile*/
+
+
 //lisab vormist saadud andmed muutujasse
 $a = $_GET['t1'];
 $b = $_GET['t2'];
 $h = $_GET['t3'];
+$c = $_GET['t4'];
 
 //trapetsi pindala arvutamine
 $pindala = (($a + $b)/2) * $h;
+//rombi ümbermõõdu arvutamine
+$umbermoot = $c * 4;
 
-printf('Trapetsi pindala on: %0.2f cm2', $pindala);
+printf('Trapetsi pindala on %0.2f <br>', $pindala);
+echo 'Rombi pindala on '.$umbermoot;
+
 
 ?>
