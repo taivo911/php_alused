f79441235b3333974ceb73f137eabada8a41f264
taivo911 <taivo.nurk@khk.ee>
2020-12-15 15:07:06 +0200

    kontakt ül 10

---
+++ kontakt.php
@@ -0,0 +1,7 @@
+<h2>Avaleht</h2>
+<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lacinia feugiat mi,
+    ac blandit purus hendrerit vel. Praesent at gravida libero. Nulla mollis, mauris sit amet
+    ullancorper tincidunt, nisi nulla rhoncus nulla, sed vestibulum augue risus a sem. Nam
+    aliquet lacus id justo viverra eget tincidunt augue pharetra. Aliquam erat volutpat.
+    Phasellus id tortor purus, ut bibendum orci. Sed rhoncus tempus viverra. Ut sagittis augue
+    eu nunc sollicitudin feugiat. </p>
